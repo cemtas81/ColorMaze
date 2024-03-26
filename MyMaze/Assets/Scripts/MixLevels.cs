1e5dbf94bf46db19898cf3df3a1c171fe2d4b652
cemtas81 <79138234+cemtas81@users.noreply.github.com>
2024-03-26 16:34:08 +0300

    audio mixer

---
+++ MyMaze/Assets/Scripts/MixLevels.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class MixLevels : MonoBehaviour {
+
+	public AudioMixer masterMixer;
+
+	public void SetSfxLvl(float sfxLvl)
+	{
+		masterMixer.SetFloat("SfxVol", Mathf.Log10(sfxLvl));
+	}
+
+	public void SetMusicLvl (float musicLvl)
+	{
+		masterMixer.SetFloat ("MusicVol",Mathf.Log10( musicLvl));
+	}
+}
